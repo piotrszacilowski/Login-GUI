7e990efc82de893d5d70858b0359392fd4a9e4ee
piotrszacilowski <szacilowski.piotr@gmail.com>
2020-02-14 19:45:27 +0100

    Console printout via eventListener

--- src/GIU.java
+++ src/GIU.java
@@ -2,47 +2,55 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI implements ActionListener {
+class GUI implements ActionListener {
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passwordLabel;
+    private static JPasswordField passwordText;
+    private static JButton button;
+    private static JLabel success;
+
     public static void main(String[] args) {
         JPanel panel = new JPanel();
         JFrame frame = new JFrame();
-        frame.setSize(350,200);
+        frame.setSize(350, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
 
         panel.setLayout(null);
 
-        JLabel userLabel = new JLabel("User");
-        userLabel.setBounds(10,20,80,25);
+        userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25);
         panel.add(userLabel);
 
-        JTextField userTest = new JTextField();
-        userTest.setBounds(100,20,165,25);
-        panel.add(userTest);
+        userText = new JTextField();
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
 
-        JLabel passwordLabel = new JLabel("Password");
-        passwordLabel.setBounds(10,50,80,25);
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
         panel.add(passwordLabel);
 
-        JPasswordField passwordText = new JPasswordField();
-        passwordText.setBounds(100,50,165,25);
+        passwordText = new JPasswordField();
+        passwordText.setBounds(100, 50, 165, 25);
         panel.add(passwordText);
 
-        JButton button = new JButton("Login");
-        button.setBounds(10,80,80,25);
+        button = new JButton("Login");
+        button.setBounds(10, 80, 80, 25);
         button.addActionListener(new GUI());
         panel.add(button);
 
-        JLabel success = new JLabel("");
-        success.setBounds(10,110,300,25);
+        success = new JLabel("");
+        success.setBounds(10, 110, 300, 25);
         panel.add(success);
-        success.setText();
 
         frame.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        String user = userText.getText();
+        String password = passwordText.getText();
+        System.out.println(user + ", " + password);
     }
 }
